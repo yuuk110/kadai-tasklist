85431b76e6689450c35e2f5fb9b19e87a2eca32e
yuuki.suzuki <yuuki_suzuki@tglab.jp>
2022-08-24 01:24:47 +0000

    tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,6 +5,23 @@
 <!-- ここにページ毎のコンテンツを書く -->
 
 <h1>タスク一覧</h1>
-@if ()
+@if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{{ $task->id }}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -12,9 +12,17 @@
             <td>{{ $task->id }}</td>
         </tr>
         <tr>
-            <th>メッセージ</th>
+            <th>タスク</th>
             <td>{{ $task->content }}</td>
         </tr>
     </table>
+    
+    {{-- メッセージ編集ページへのリンク --}}
+    {!! link_to_route('tasks.edit', 'このメッセージを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
+
+    {{-- メッセージ削除フォーム --}}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+    {!! Form::close() !!}
 
 @endsection
