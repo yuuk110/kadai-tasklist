068c8d50c407d9b8f2b53b58e422622bfaf887bc
yuuki.suzuki <yuuki_suzuki@tglab.jp>
2022-08-31 22:55:26 +0000

    user login

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,21 +1,33 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        {{-- トップページへのリンク --}}
         <a class="navbar-brand" href="/">TaskList</a>
-
+         
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
         </button>
-
+        
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                {{-- メッセージ作成ページへのリンク --}}
-                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li
-                {{-- ユーザ登録ページへのリンク --}}
-                <li class="nav-item"></li>
-                {{-- ログインページへのリンク --}}
-                <li class="nav-item"></li>
+            
+            @if (Auth::check()) 
+                <li class="nav-item dropdown"> 
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                    <ul class="dropdown-menu dropdown-menu-right">
+                    <li class="dropdown-divider"></li>
+                        <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                    </ul>
+                <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
+                </li>
+            @else
+                    
+                
+                 <ul class="nav navbar-nav navbar-right">
+                    <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                </ul>
+             @endif
+                
             </ul>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+  @if (Auth::check())
+     {{ Auth::user()->name }}
+  @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the TaskList</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+  @endif
 @endsection
