ec4f3bb5cd902440d0421fdc12d1d46ab1305b14
yuuki.suzuki <yuuki_suzuki@tglab.jp>
2022-09-01 10:18:10 +0000

    task

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -20,7 +20,7 @@
             <td>{{ $task->content }}</td>
         </tr>
     </table>
-    {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-light']) !!}
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light']) !!}
 
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
